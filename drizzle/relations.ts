95f251db81f12b03ba40d69d6d1e4382b709f098
kaedenise <kayladesanders@gmail.com>
2026-04-16 18:57:22 -0500

    Update relations.ts

--- drizzle/relations.ts
+++ drizzle/relations.ts
@@ -1,1 +1,17 @@
 import {} from "./schema";
+import { relations } from "drizzle-orm";
+import { users, leaderboard } from "./schema";
+
+export const usersRelations = relations(users, ({ one }) => ({
+  leaderboardEntry: one(leaderboard, {
+    fields: [users.id],
+    references: [leaderboard.userId],
+  }),
+}));
+
+export const leaderboardRelations = relations(leaderboard, ({ one }) => ({
+  user: one(users, {
+    fields: [leaderboard.userId],
+    references: [users.id],
+  }),
+}));
